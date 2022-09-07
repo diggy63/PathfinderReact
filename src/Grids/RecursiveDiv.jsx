ff89cc525412c568e8698e6e030941a3e4537be0
diggyben <daniel.kanczugowski@gmail.com>
2022-09-07 10:25:19 -0700

    made first recursive disivion passs

--- src/Grids/RecursiveDiv.jsx
+++ src/Grids/RecursiveDiv.jsx
@@ -1,26 +1,51 @@
 export default function RecursiveDiv(props) {
   let grid = gridInit(props[0], props[1]);
-  const newGrid = Division(grid);
-  return newGrid;
+  const rowL = [0,props[0]]
+  const colL = [0,props[1]]
+  Division(rowL,colL,grid);
+  return grid;
 }
 
-function Division(grid) {
-  const row = grid.length;
-  const col = grid[0].length;
-  const wallWithGap = (Math.floor(Math.random() * (col-1))+1);
-  grid.forEach((row, i) => {
-    row.forEach((node, nodeI) => {
-      if (!node.isGap) {
-        if (i === 0 || i === grid.length - 1) {
-          node.isWall = true;
-        } else if (nodeI === 0 || nodeI === row.length - 1 || nodeI === wallWithGap) {
-          node.isWall = true;
-        }
-      }
-    });
+function Division(rows,cols,grid) {
+  let newColsA = cols
+  let newRowsA = rows
+  let newColsB = cols
+  let newRowsB = rows
+  if(rows[1]-rows[0] < 3 || cols[1]-cols[2] < 3){
+    return
+  }
+  //makes choice between vertical or horizontal serperation using 1 as horizontal and 0 and vertical
+  const rowOrCol = Math.floor(Math.random()*2)
+  //builds wall around the new grid
+  for(let rL = rows[0]; rL < rows[1]; rL++){
+    for(let cL = cols[0];cL < cols[1]; cL++){
+      if(rL === 0 || cL === 0 || rL === rows[1]-1 || cL === cols[1]-1){
+        grid[rL][cL].isWall = true
+       }
+    }
+  }
 
-  });
-  return grid;
+  if(rowOrCol > 0){
+    //builds vertical wall
+    const wall = Math.floor(Math.random()*(cols[1]-3))+2
+    for(let i  = rows[0]; i < rows[1]; i++){
+      grid[i][wall].isWall = true
+    }
+    newRowsA = [rows[0],wall]
+    newRowsB = [wall,rows[1]]
+  }else{
+    //build horizontal wall
+    const wall = Math.floor(Math.random()*(rows[1]-3))+2
+    for(let i  = cols[0]; i < cols[1]; i++){
+      grid[wall][i].isWall = true
+    }
+    newColsA = [cols[0],wall]
+    newColsB = [wall,cols[1]]
+  }
+  console.log(newRowsA,newColsA)
+  console.log(newRowsB,newColsB)
+  // Division(newRowsA,newColsA,grid)
+  // Division(newRowsB,newColsB,grid)
 }
 
 function gridInit(ROWS, COLS) {
