db1355398e2a1ba85aaade73e980aa970c4c6c07
diggyben <daniel.kanczugowski@gmail.com>
2022-09-07 13:16:08 -0700

    recursive for path a is almost done i need to figure out how to make path b work

--- src/Grids/RecursiveDiv.jsx
+++ src/Grids/RecursiveDiv.jsx
@@ -1,51 +1,87 @@
 export default function RecursiveDiv(props) {
   let grid = gridInit(props[0], props[1]);
-  const rowL = [0,props[0]]
-  const colL = [0,props[1]]
-  Division(rowL,colL,grid);
+  const rowL = [0, props[0]];
+  const colL = [0, props[1]];
+  Division(rowL, colL, grid);
   return grid;
 }
 
-function Division(rows,cols,grid) {
-  let newColsA = cols
-  let newRowsA = rows
-  let newColsB = cols
-  let newRowsB = rows
-  if(rows[1]-rows[0] < 3 || cols[1]-cols[2] < 3){
-    return
+function Division(rows, cols, grid) {
+  console.log(rows,cols)
+  let vert = false
+  let horz = false
+  let newColsA = cols;
+  let newRowsA = rows;
+  let newColsB = cols;
+  let newRowsB = rows;
+
+  if (rows[1] - rows[0] < 4 || cols[1] - cols[0] < 4) {
+    console.log('done')
+    return;
   }
   //makes choice between vertical or horizontal serperation using 1 as horizontal and 0 and vertical
-  const rowOrCol = Math.floor(Math.random()*2)
+  const rowOrCol = Math.floor(Math.random() * 2);
+  rowOrCol === 1 ? vert = true : horz = true
+  //check to make sure if we need to go horizonatal or vertical to ignore the random flip
+  if((cols[1] - cols[0]) < 5){
+    horz = true
+    vert = false
+  }else if((rows[1] - rows[0] < 5 )){
+    vert = true
+    horz = false
+  }
   //builds wall around the new grid
-  for(let rL = rows[0]; rL < rows[1]; rL++){
-    for(let cL = cols[0];cL < cols[1]; cL++){
-      if(rL === 0 || cL === 0 || rL === rows[1]-1 || cL === cols[1]-1){
-        grid[rL][cL].isWall = true
-       }
-    }
+  for (let rL = rows[0]; rL < rows[1]; rL++) {
+    for (let cL = cols[0]; cL < cols[1]; cL++) {
+      if (rL === 0 || cL === 0 || rL === rows[1] - 1 || cL === cols[1] - 1) {
+        if(grid[rL][cL].isGap){
+        }else if (!grid[rL][cL].isWall) {
+            grid[rL][cL].isWall = true;
+          }
+          
+        }
+      }
+    
   }
-
-  if(rowOrCol > 0){
+  if (vert) {
     //builds vertical wall
-    const wall = Math.floor(Math.random()*(cols[1]-3))+2
-    for(let i  = rows[0]; i < rows[1]; i++){
-      grid[i][wall].isWall = true
+    
+    let wall = Math.floor(Math.random() * (cols[1] - 4)) + 2;
+    const gap = Math.floor(Math.random() * (rows[1] - 4)) + 2;
+    for (let i = rows[0]; i < rows[1]; i++) {
+      if(grid[i][wall].isGap && (wall + 3) >= cols[1]){
+        wall = wall - 1
+      }else if(grid[i][wall].isGap){
+        wall = wall + 1
+      }
     }
-    newRowsA = [rows[0],wall]
-    newRowsB = [wall,rows[1]]
-  }else{
+    for (let i = rows[0]; i < rows[1]; i++) {
+      if(i === gap){
+        grid[i][wall].isGap = true;
+      }else{
+        grid[i][wall].isWall = true;
+      }
+      
+    }
+    newColsA = [cols[0], wall + 1];
+    newColsB = [wall, cols[1]];
+  } else if(horz) {
     //build horizontal wall
-    const wall = Math.floor(Math.random()*(rows[1]-3))+2
-    for(let i  = cols[0]; i < cols[1]; i++){
-      grid[wall][i].isWall = true
+    const wall = Math.floor(Math.random() * (rows[1] - 4)) + 2;
+    const gap = Math.floor(Math.random() * (cols[1] - 4)) + 2;
+    for (let i = cols[0]; i < cols[1]; i++) {
+      if(i === gap){
+        grid[wall][i].isGap = true;
+      }else{
+        grid[wall][i].isWall = true;
+      }
+      
     }
-    newColsA = [cols[0],wall]
-    newColsB = [wall,cols[1]]
+    newRowsA = [rows[0], wall + 1];
+    newRowsB = [wall, rows[1]];
   }
-  console.log(newRowsA,newColsA)
-  console.log(newRowsB,newColsB)
-  // Division(newRowsA,newColsA,grid)
-  // Division(newRowsB,newColsB,grid)
+  Division(newRowsA,newColsA,grid)
+
 }
 
 function gridInit(ROWS, COLS) {
