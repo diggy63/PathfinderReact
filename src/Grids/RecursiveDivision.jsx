9246d41b5c09c64c9354fdb69c19b87964f26d32
diggyben <daniel.kanczugowski@gmail.com>
2022-09-22 14:02:06 -0700

    so close to finsihing the gap finder problem need a break

--- src/Grids/RecursiveDivision.jsx
+++ src/Grids/RecursiveDivision.jsx
@@ -13,23 +13,42 @@ export default function RecursiveDivision(props) {
 }
 //recursive division function
 function division(rows, cols, grid, np) {
-  console.log(cols)
-  if(cols[1]-cols[0] <= 0){
+  console.log('start of div', rows, cols)
+  if(cols[1]-cols[0] <= 0 || !cols){
+    console.log('cols sqiushed')
     return
   }
-  if(rows[1]-rows[0] <= 0){
+  if(rows[1]-rows[0] <= 0 || !rows){
+    console.log('rows squished')
+    return
+  }
+  if(cols[1]-cols[0] === 1 && rows[1] - rows[0] === 1){
+    finishSquare(rows,cols,grid,np)
     return
   }
   const direct = getWallDirection(rows,cols)
   // const direct = true
   if(direct){
-    const newCols = buildVert(rows,cols, grid, np)
-    division(rows,newCols[0],grid,np)
-    division(rows,newCols[1],grid,np)
+    let newCols = buildVert(rows,cols, grid, np)
+    if(!newCols){
+      newCols = division(rows, cols, grid, np)
+    }
+    division(newCols[0],newCols[2],grid,np)
+    division(newCols[1],newCols[3],grid,np)
   }else{
-    const newRows = buildHorz(rows,cols, grid, np)
-    division(newRows[0],cols,grid,np)
-    division(newRows[1],cols,grid,np)
+    let newRows = buildHorz(rows,cols, grid, np)
+    if(!newRows){
+      newRows = division(rows, cols, grid, np)
+    }
+    // if(newRows === 'done'){
+    //   console.log('done with rect')
+    //   return
+    // }
+    // if(newRows.length === 0){
+    //   division(rows, cols, grid, np)
+    // }
+    division(newRows[0],newRows[2],grid,np)
+    division(newRows[1],newRows[3],grid,np)
     
   }
 }
@@ -97,25 +116,44 @@ function getWallDirection(rows, cols) {
 }
 
 function buildHorz(rows, cols, grid, np) {
+  console.log('builidng horz')
   let wall = Math.floor(Math.random()*(rows[1]-rows[0]-1))+(rows[0]+1)
+  // let wall = Math.floor(((rows[1]-rows[0]+1)/2)+(rows[0]+1))
   const gap = Math.floor(Math.random()*(cols[1]-cols[0]+1)+(cols[0]))
   if(grid[wall][cols[0]-1].isGap || grid[wall][cols[1]+1].isGap ){
-    
+    console.log(grid[wall][cols[0]-1])
+    console.log(grid[wall][cols[1]+1])
+    // if(wall + 1 === rows[1]){
+    //   wall = wall - 1
+    // }else{
+    //   wall = wall + 1
+    // }
+    return false
   }
   for( let i = cols[0]; i <= cols[1]; i++){
     if(gap === i){
       grid[wall][i].isGap = true
-      console.log(gap)
     }else{
       grid[wall][i].isWall = true
     }
     
   }
-  return [[rows[0],wall-1],[wall+1,rows[1]]]
+  return [[rows[0],wall-1],[wall+1,rows[1]],[cols[0],cols[1]],[cols[0],cols[1]]]
 }
 function buildVert(rows, cols, grid, np) {
+  console.log('building vert')
   let wall = Math.floor(Math.random()*(cols[1]-cols[0]-1))+(cols[0]+1)
+  // let wall = Math.floor(((cols[1]-cols[0]+1)/2)+(cols[0]+1))
   const gap = Math.floor(Math.random()*(rows[1]-rows[0]+1)+(rows[0]))
+  if(grid[rows[0]-1][wall].isGap || grid[rows[1]+1][wall].isGap ){
+    // if(wall + 1 === cols[1]){
+    //   wall = wall - 1
+    // }
+    // else{
+    //   wall = wall + 1
+    // }
+    return false
+  }
   for( let i = rows[0]; i <= rows[1]; i++){
     if(gap === i){
       grid[i][wall].isGap = true
@@ -123,5 +161,10 @@ function buildVert(rows, cols, grid, np) {
       grid[i][wall].isWall = true
     }
   }
-  return [[cols[0],wall-1],[wall+1,cols[1]]]
+  return [[rows[0],rows[1]],[rows[0],rows[1]],[cols[0],wall-1],[wall+1,cols[1]]]
+}
+
+function finishSquare(rows,cols,grid,np){
+  console.log(rows)
+  console.log(cols)
 }
