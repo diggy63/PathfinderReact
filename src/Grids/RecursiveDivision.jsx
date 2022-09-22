5ded629472d6996dcad47df6f59ef866db08e99b
diggyben <daniel.kanczugowski@gmail.com>
2022-09-22 12:21:16 -0700

    recursive maze build ing works on both paths but doesnt see the gap

--- src/Grids/RecursiveDivision.jsx
+++ src/Grids/RecursiveDivision.jsx
@@ -6,27 +6,34 @@ export default function RecursiveDivision(props) {
   const colsInit = [0, props[1]];
   const nodesProcessed = [];
   divisionInit(rowsInit, colsInit, grid, nodesProcessed);
-  division(rowsInit, colsInit, grid, nodesProcessed);
+  const divRows = [1,(props[0]-2)]
+  const divCols = [1,(props[1]-2)]
+  division(divRows, divCols, grid, nodesProcessed);
   return [grid, nodesProcessed];
 }
-
+//recursive division function
 function division(rows, cols, grid, np) {
-  if (rows[1] - rows[0] <= 3 || cols[1] - rows[0] <= 3) {
-    return;
+  console.log(cols)
+  if(cols[1]-cols[0] <= 0){
+    return
   }
-  const direc = getWallDirection(rows, cols);
-  //   const direc = true
-  if (direc) {
-    const newCols = buildVert(rows, cols, grid, np);
+  if(rows[1]-rows[0] <= 0){
+    return
+  }
+  const direct = getWallDirection(rows,cols)
+  // const direct = true
+  if(direct){
+    const newCols = buildVert(rows,cols, grid, np)
     division(rows,newCols[0],grid,np)
-    // division(rows, newCols[1], grid, np);
-  } else {
-    const newRols = buildHorz(rows, cols, grid, np);
-    division(newRols[0],cols,grid,np)
-    // division(newRols[1], cols, grid, np);
+    division(rows,newCols[1],grid,np)
+  }else{
+    const newRows = buildHorz(rows,cols, grid, np)
+    division(newRows[0],cols,grid,np)
+    division(newRows[1],cols,grid,np)
+    
   }
 }
-
+// makes border aoround pathfinder area
 function divisionInit(rows, cols, grid, np) {
   for (let i = 0; i < cols[1]; i++) {
     grid[0][i].isWall = true;
@@ -45,7 +52,7 @@ function divisionInit(rows, cols, grid, np) {
     np.push([i, 0]);
   }
 }
-
+//builds the array of arrays that represent our nodes to do recursive div
 function gridInit(ROWS, COLS) {
   const nodestart = [];
   for (let row = 0; row < ROWS; row++) {
@@ -74,77 +81,47 @@ function createNode(col, row) {
 }
 
 function getWallDirection(rows, cols) {
-  if (rows[1] - rows[0] === 4) {
-    return true;
-  } else if (cols[1] - cols[0] === 4) {
-    return false;
-  }
-  let number = Math.random();
-  if (number < 0.5) {
-    return true;
-  } else {
-    return false;
-  }
+    if(cols[1] - cols[0] <= 1){
+      return false
+    }
+    if(rows[1] - rows[0] <= 1){
+      return true
+    }
+    const x = (Math.floor(Math.random() * 2) == 0);
+    if(x){
+    	return true;
+    }else{
+      return false;
+    }
+
 }
 
 function buildHorz(rows, cols, grid, np) {
-  const wallEnd = cols[1] - 2;
-  const innerWallLength = rows[1] - rows[0] - 4;
-  let wallPosition =
-    Math.floor(Math.random() * innerWallLength) + (2 + rows[0]);
-  const gapInWall =
-    Math.floor(Math.random() * (wallEnd - cols[0])) + (1 + cols[0]);
-  console.log(grid[wallPosition][cols[0]], grid[wallPosition][cols[1] - 1]);
-  if (
-    grid[wallPosition][cols[0]].isGap ||
-    grid[wallPosition][cols[1] - 1].isGap
-  ) {
-    if (wallPosition + 3 === cols[1]) {
-      wallPosition = wallPosition - 1;
-    } else {
-      wallPosition = wallPosition + 1;
-    }
+  let wall = Math.floor(Math.random()*(rows[1]-rows[0]-1))+(rows[0]+1)
+  const gap = Math.floor(Math.random()*(cols[1]-cols[0]+1)+(cols[0]))
+  if(grid[wall][cols[0]-1].isGap || grid[wall][cols[1]+1].isGap ){
+    
   }
-  for (let i = cols[0] + 1; i <= wallEnd; i++) {
-    if (gapInWall === i) {
-      grid[wallPosition][i].isGap = true;
-    } else {
-      grid[wallPosition][i].isWall = true;
-      np.push([[wallPosition], i]);
+  for( let i = cols[0]; i <= cols[1]; i++){
+    if(gap === i){
+      grid[wall][i].isGap = true
+      console.log(gap)
+    }else{
+      grid[wall][i].isWall = true
     }
+    
   }
-  return [
-    [rows[0], wallPosition + 1],
-    [wallPosition, rows[1]],
-  ];
+  return [[rows[0],wall-1],[wall+1,rows[1]]]
 }
 function buildVert(rows, cols, grid, np) {
-  const wallEnd = rows[1] - 2;
-  const innerWallLength = cols[1] - cols[0] - 4;
-  let wallPosition =
-    Math.floor(Math.random() * innerWallLength) + (2 + cols[0]);
-  const gapInWall =
-    Math.floor(Math.random() * (wallEnd - rows[0])) + (1 + rows[0]);
-  if (
-    grid[rows[0]][wallPosition].isGap ||
-    grid[rows[1] - 1][wallPosition].isGap
-  ) {
-    if (wallPosition + 3 === rows[1]) {
-      wallPosition = wallPosition - 1;
-    } else {
-      wallPosition = wallPosition + 1;
+  let wall = Math.floor(Math.random()*(cols[1]-cols[0]-1))+(cols[0]+1)
+  const gap = Math.floor(Math.random()*(rows[1]-rows[0]+1)+(rows[0]))
+  for( let i = rows[0]; i <= rows[1]; i++){
+    if(gap === i){
+      grid[i][wall].isGap = true
+    }else{
+      grid[i][wall].isWall = true
     }
   }
-  for (let i = rows[0] + 1; i <= wallEnd; i++) {
-    if (gapInWall === i) {
-      grid[i][wallPosition].isGap = true;
-    } else {
-      grid[i][wallPosition].isWall = true;
-      np.push([i, [wallPosition]]);
-    }
-  }
-  return [
-    [cols[0], wallPosition + 1],
-    [wallPosition, cols[1], grid],
-  ];
+  return [[cols[0],wall-1],[wall+1,cols[1]]]
 }
